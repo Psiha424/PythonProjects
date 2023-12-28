71ab379e8e9009a095f7d8f6d3099980c19665d7
Const <psiha424@gmail.com>
2023-12-28 23:05:09 +0800

    the 1st commit

---
+++ 1hw/hw.py
@@ -0,0 +1,30 @@
+# n = int(123456)
+
+# # Введите ваше решение ниже
+# map(int, str(n))
+# l = int(n[0]) + int(n[1]) + int(n[2])
+# r = int(n[3]) + int(n[4]) + int(n[5])
+# if l == r:
+#     print('Yes')
+# else:
+#     print('NO')
+# n = 123322
+# num_list = list(map(int, str(n)))
+# print(num_list)
+# l = int(num_list[0]) + int(num_list[1]) + int(num_list[2])
+# print(l)
+# r = int(num_list[3]) + int(num_list[4]) + int(num_list[5])
+# print(r)
+# if r == l: 
+#     print('Yes')
+
+# else:
+#     print('no')
+
+
+# n,m,k = int(input('В-те 1-ю сторону: ')),int(input('В-те 2-ю сторону: ')),int(input('В-те кол-во долек: '))
+# if k%n == 0 or k%m == 0:
+#     print('Yes')
+# else: print('No')
+# # или
+# if k < n*m and (k % n == 0 or k % m == 0):
