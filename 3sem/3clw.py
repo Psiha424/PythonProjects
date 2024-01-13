a2d4feeb040d7926b6f3f97f8b63797bb29b7999
Const <psiha424@gmail.com>
2024-01-13 19:19:49 +0800

    3 задача 3 семинара

---
+++ 3sem/3clw.py
@@ -0,0 +1,26 @@
+# my_dict = [1, 1, 2, 0, -1, 3, 4, 4]
+# result = []
+# for i in my_dict:
+#     if i not in result:
+#         result.append(i)
+# print(result)
+# print(len(result))        
+
+
+# # num = [1, 2, 3, 4, 5]
+# # k = 3
+# # print (num[-k-1:] + num[:-k-1])
+# # print (num[-k-1:])
+# # print (num[:-k-1])
+
+
+# num = [1, 2, 3, 4, 5]
+# k = 4
+# k = k % len(num)
+# while k > 0:
+#     temp = num.pop()
+#     num.insert(0, temp)
+#     k -=1
+# print(num) 
+
+
